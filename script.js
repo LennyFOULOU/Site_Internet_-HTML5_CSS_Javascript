a3b7f3e09f758296adabe8467dd772da9760b1c7
foulou.salle224-15 <lenny.foulou@etu.u-pec.fr>
2024-01-17 20:42:02 +0100

    amélioration page d'accueil

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+document.addEventListener("DOMContentLoaded", function () {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
 
+    function openLoginForm(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        wrapper.classList.add('active-popup');
+    }
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
+    function closeLoginForm(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        wrapper.classList.remove('active-popup');
+    }
+
+    registerLink.addEventListener('click', function (event) {
+        openLoginForm(event);
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', function (event) {
+        openLoginForm(event);
+        wrapper.classList.remove('active');
+    });
+
+    btnPopup.addEventListener('click', openLoginForm);
+
+    iconClose.addEventListener('click', closeLoginForm);
 });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
+window.addEventListener('scroll', function() {
+    var headerPlaceholder = document.querySelector('.header-placeholder');
+    var header = document.querySelector('header');
+    
+    if (window.scrollY >= header.offsetTop) {
+        headerPlaceholder.style.display = 'block';
+    } else {
+        headerPlaceholder.style.display = 'none';
+    }
 });
