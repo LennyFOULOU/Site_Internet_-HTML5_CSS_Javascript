3025684d7a555c272a912b868dc6ff0aaa5b8dd4
foulou.salle224-15 <lenny.foulou@etu.u-pec.fr>
2024-01-19 03:17:54 +0100

    Ajouts de nouvelles pages sur le site

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const wrapper = document.querySelector('.wrapper');
-    const loginLink = document.querySelector('.login-link');
-    const registerLink = document.querySelector('.register-link');
-    const btnPopup = document.querySelector('.btnLogin-popup');
-    const iconClose = document.querySelector('.icon-close');
 
-    function openLoginForm(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        wrapper.classList.add('active-popup');
+    var mybutton = document.getElementById("backToTopBtn");
+
+    // Afficher le bouton lorsqu'on descend en dessous de 20 pixels depuis le haut de la page
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
     }
 
-    function closeLoginForm(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        wrapper.classList.remove('active-popup');
-    }
-    function hideLoginForm() {
-        wrapper.classList.remove('active-popup');
+   
+    mybutton.onclick = function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    };
+
+    function printCV(cvUrl) {
+        var printWindow = window.open(cvUrl, '_blank');
+        
+        printWindow.onload = function () {
+            printWindow.print();
+            printWindow.onafterprint = function () {
+                printWindow.close();
+            };
+        };
     }
 
-    registerLink.addEventListener('click', function (event) {
-        openLoginForm(event);
-        wrapper.classList.add('active');
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const mobileMenu = document.querySelector('.mobile-menu');
+    
+        menuToggle.addEventListener('click', function () {
+            mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'flex' : 'none';
+        });
     });
-
-    loginLink.addEventListener('click', function (event) {
-        openLoginForm(event);
-        wrapper.classList.remove('active');
-    });
-    closeLoginForm();
-
-    btnPopup.addEventListener('click', openLoginForm);
-    iconClose.addEventListener('click', closeLoginForm);
-});
-
